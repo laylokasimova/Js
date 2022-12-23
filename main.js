4ab8bee8f7f9627eda6c1f19a9eb38f68b0ccf15
Laylo <laylokasimovaa@gmail.com>
2022-12-23 19:44:30 +0500

    Global Version

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-console.log(10 + 5);
-console.log(30 - 5);
-console.log(100 + 250);
-console.log(14 / 8);
-console.log(20 * 20);
-console.log(20 % 5);
 
-console.log('10 + 5 = ' + (10 + 5));
-console.log('30 - 5 = ' + (30 - 5));
-console.log('100 + 250 = ' + (100 + 250));
-console.log('14 / 8= ' + (14 / 8));
-console.log('20 * 20= ' + (20 * 20));
-console.log('20 % 5 = ' + (20 % 5));
+var num1 =prompt('6 + 3 = ?')
+alert('sizning javobingiz ' + num1 + ' togri javob esa ' + (6 + 3));   
 
+var num2 =prompt('10 + 5 = ?')
+alert('sizning javobingiz ' + num2 + ' togri javob esa ' + (10 + 5));  
 
-var num =prompt('6 + 3 = ?')
-console.log(num);
-alert('sizning javobingiz ' + num + ' togri javob esa 9')   
+var num3 =prompt('100 + 250 = ?')
+alert('sizning javobingiz ' + num3 + ' togri javob esa ' + (100 + 250)); 
+
+var num4 =prompt('14 / 2 = ?')
+alert('sizning javobingiz ' + num4 + ' togri javob esa ' + (14 / 2));   
+
+var num5 =prompt('20 * 20 = ?')
+alert('sizning javobingiz ' + num5 + ' togri javob esa ' + (20 * 20)); 
+
+var num6 =prompt('10 + 20 = ?')
+alert('sizning javobingiz ' + num6 + ' togri javob esa ' + (10 + 20));   
