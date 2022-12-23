c4ca7218f4810b214b9c5c53570b78395627d81f
Laylo <laylokasimovaa@gmail.com>
2022-12-23 18:47:33 +0500

    Global Version

---
+++ main.js
@@ -0,0 +1,18 @@
+console.log(10 + 5);
+console.log(30 - 5);
+console.log(100 + 250);
+console.log(14 / 8);
+console.log(20 * 20);
+console.log(20 % 5);
+
+console.log('10 + 5 = ' + (10 + 5));
+console.log('30 - 5 = ' + (30 - 5));
+console.log('100 + 250 = ' + (100 + 250));
+console.log('14 / 8= ' + (14 / 8));
+console.log('20 * 20= ' + (20 * 20));
+console.log('20 % 5 = ' + (20 % 5));
+
+
+var num =prompt('6 + 3 = ?')
+console.log(num);
+alert('sizning javobingiz ' + num + ' togri javob esa 9')   
